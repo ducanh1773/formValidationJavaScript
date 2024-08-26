421aaffa00763c32e044239faebeeacacf079283
ducanh1773 <anhductrannguyen170703@gmail.com>
2024-08-26 11:38:10 +0700

    main

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,16 +1,28 @@
 function Validator(options){
     var formElement = document.querySelector(options.form);
     if(formElement){
-       
+       options.rules.forEach(function(rule){
+            var inputElemnt = formElement.querySelector(rule.selector);
+        if(inputElemnt){
+            inputElemnt.onblur = function(){
+             console.log(inputElemnt.value)
+             var errorMessage = rule.test(inputElemnt.value);
+             console.log(errorMessage);
+             if(errorMessage)
+            }
+        }
+       })
     }
 }
 
-
+//Nguyen tac cua cac rule
+//1. KHi co loi => tra ra message loi
+//2. Khi hop le => undefined
 Validator.isRequired = function(selector){
     return{
     selector:selector,
-    test: function(){
-        
+    test: function(value){
+        return value.trim() ? undefined : 'Vui long nhap truong nay';
     }
     };
 }
@@ -18,7 +30,7 @@ Validator.isRequired = function(selector){
 Validator.isEmail = function(selector){
     return{
         selector:selector,
-        test: function(){
+        test: function(value){
             
         }
         };
